d67dffae64a6c4ab634961afc2047cead9e96010
EchoinInk <alex@echoin.ink>
2026-05-24 07:46:12 +1200

    chore(lumo): refine system (2 files)

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,9 @@
-import { Redirect } from "expo-router";
+import { View, Text } from "react-native";
 
 export default function RootIndex() {
-  return <Redirect href="/(tabs)" />;
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>ROOT IS RENDERING</Text>
+    </View>
+  );
 }
