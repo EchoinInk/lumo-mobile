eef226d63a86ad770d4c5e7ac937ebcec68b76ae
EchoinInk <alex@echoin.ink>
2026-05-24 07:56:56 +1200

    chore(lumo): refine system (2 files)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,5 @@
-import "@/src/global.css";
-import { SyncProvider } from "@/src/providers/SyncProvider";
-import { useAuthStore } from "@/src/store/useAuthStore";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const hasHydrated = useAuthStore((state) => state.hasHydrated);
-
-  if (!hasHydrated) {
-    return null;
-  }
-
-  return (
-    <SyncProvider>
-      <Stack screenOptions={{ headerShown: false }} />
-    </SyncProvider>
-  );
+  return <Stack />;
 }
