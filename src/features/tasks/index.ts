64a83c08de8a1119219486f219393c06370e5f1b
EchoinInk <alex@echoin.ink>
2026-05-26 07:13:34 +1200

    chore(lumo): update index.ts

--- src/features/tasks/index.ts
+++ src/features/tasks/index.ts
@@ -2,26 +2,23 @@
 // Local-first task management with calm UX
 
 // Components
-export { AddTaskButton } from './components/AddTaskButton';
-export { AddTaskModal } from './components/AddTaskModal';
-export { EmptyTasks } from './components/EmptyTasks';
-export { TaskFilterPills } from './components/TaskFilterPills';
-export { TaskList } from './components/TaskList';
-export { TaskRow } from './components/TaskRow';
-export { TaskSection } from './components/TaskSection';
+export { AddTaskButton } from "./components/AddTaskButton";
+export { AddTaskModal } from "./components/AddTaskModal";
+export { EmptyTasks } from "./components/EmptyTasks";
+export { TaskFilterPills } from "./components/TaskFilterPills";
+export { TaskFormModal } from "./components/TaskFormModal";
+export { TaskList } from "./components/TaskList";
+export { TaskRow } from "./components/TaskRow";
+export { TaskSection } from "./components/TaskSection";
 
 // Hooks
-export { useTasks } from './hooks/useTasks';
+export { useTasks } from "./hooks/useTasks";
 
 // Types
 export type {
-  Task,
-  TaskPriority,
-  TaskStatus,
-  TaskFilter,
-  CreateTaskInput,
-  UpdateTaskInput,
-} from './types/task';
+    CreateTaskInput, Task, TaskFilter, TaskPriority,
+    TaskStatus, UpdateTaskInput
+} from "./types/task";
 
 // Store (for advanced use cases)
-export { useTaskStore } from './store/useTaskStore';
+export { useTaskStore } from "./store/useTaskStore";
