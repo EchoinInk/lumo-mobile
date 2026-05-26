0abd301e8415656b3043a0afc8d75e7556f167f8
EchoinInk <alex@echoin.ink>
2026-05-27 00:39:42 +1200

    chore(lumo): update useHabitStore.ts

--- src/store/useHabitStore.ts
+++ src/store/useHabitStore.ts
@@ -1,5 +1,5 @@
+import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
-import { create } from "zustand/react";
 import { createPersistStorage } from "./createPersistStorage";
 
 export interface Habit {
