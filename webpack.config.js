2e85ccc290ddc120edb46de0048c8996d2a5dcdd
EchoinInk <alex@echoin.ink>
2026-05-26 15:37:16 +1200

    chore(lumo): update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,5 @@ const createExpoWebpackConfigAsync = require("@expo/webpack-config");
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
 
-  // Force environment variables
-  process.env.EXPO_USE_HERMES = "false";
-  process.env.EXPO_UNSTABLE_TRANSFORM_PROFILE = "default";
-
   return config;
 };
