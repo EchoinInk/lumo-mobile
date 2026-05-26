d1710da4be76efc12461a052792d495434246369
EchoinInk <alex@echoin.ink>
2026-05-26 14:21:01 +1200

    chore(lumo): update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      // Add support for import.meta in web environment
-      [
-        "babel-plugin-transform-import-meta",
-        {
-          metaProperty: "meta",
-        },
-      ],
-    ],
   };
 };
