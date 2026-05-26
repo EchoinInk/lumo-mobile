df106b7c22e22ae5eb980f37055aa1dc865e5d2d
EchoinInk <alex@echoin.ink>
2026-05-26 14:45:54 +1200

    chore(lumo): refine system (2 files)

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      // Transform import.meta to work with Hermes
+      "transform-import-meta",
+    ],
   };
 };
