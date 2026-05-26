4b7ba0b3d80ff9529a19c7dbdf367c7b13864964
EchoinInk <alex@echoin.ink>
2026-05-26 15:37:02 +1200

    chore(lumo): update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
-const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const createExpoWebpackConfigAsync = require("@expo/webpack-config");
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-  
-  // Disable Hermes and use default transform profile to avoid import.meta issues
-  config.resolve.alias = {
-    ...config.resolve.alias,
-  };
-  
+
+  // Force environment variables
+  process.env.EXPO_USE_HERMES = "false";
+  process.env.EXPO_UNSTABLE_TRANSFORM_PROFILE = "default";
+
   return config;
 };
