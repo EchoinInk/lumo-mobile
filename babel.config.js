2f314f4c2d59177240ee911f5ef17f5d2e966897
EchoinInk <alex@echoin.ink>
2026-05-26 19:31:29 +1200

    chore(lumo): update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,15 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          jsxImportSource: "nativewind",
+          unstable_transformImportMeta: true,
+        },
+      ],
+    ],
     plugins: ["nativewind/babel"],
   };
 };
