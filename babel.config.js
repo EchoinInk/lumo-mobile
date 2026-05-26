9afc98d15dee0b7b04c4dcc16ac001e520c2f854
EchoinInk <alex@echoin.ink>
2026-05-26 14:46:07 +1200

    chore(lumo): update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      // Transform import.meta to work with Hermes
-      "transform-import-meta",
-    ],
   };
 };
