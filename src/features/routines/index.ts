c2e04b553fcf8ff7345f52912304f82f4ed13323
EchoinInk <alex@echoin.ink>
2026-06-03 03:26:33 +1200

    chore(lumo): update index.ts

--- src/features/routines/index.ts
+++ src/features/routines/index.ts
@@ -1,3 +1,4 @@
 export { createTasksFromBundle } from "./services/createRoutineTasks";
+export { createRoutineBundleApplyGuard } from "./services/routineBundleApplyGuard";
 export { starterRoutineBundles } from "./services/starterBundles";
 export type { RoutineBundle, RoutineBundleItem } from "./types/routineBundle";
